b99ef0979e953b42669e16aba12430f0d56bbefe
swamyuma <swamyuma@users.noreply.github.com>
2015-09-12 06:27:30 -0500

    initial commit of mocab_commands
    I use these commands to monitor the jobs submitted to moab.

---
+++ moab_commands.sh
@@ -0,0 +1,16 @@
+# functions to be included in .bashrc
+
+# get running job id's and sort it in reverse order
+function sq() { showq| grep Running|awk '{ print $2}'| sort| uniq -c| sort -n -r; }
+
+# get the count of state of nodes where jobs are running successfully 
+function mrun() { mdiag -n | grep R| wc -l; }
+
+# get the count of busy nodes
+function mbusy() { mdiag -n | grep Busy| wc -l; }
+
+# get the count of PBS jobs that are running
+function qs() { qstat| grep [[:blank:]]R[[:blank:]]|wc -l; }
+
+# get the node name of the specified job
+function get_host() { qstat -f "$@" | grep exec_host | cut -d"=" -f2; }
